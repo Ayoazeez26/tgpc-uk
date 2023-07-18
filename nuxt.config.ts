e0fc4d2efec970c20fe1d3a57767498a4c8299b4
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2023-07-18 10:55:30 +0100

    feat: added tawk.to

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,23 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Leading management consulting company specializing in Tender, Grant, and Bid writing and management as well as developing quality assurance frameworks and enabling successful license applications.' }
+      ],
+      script: [
+        {
+          hid: 'tawk.to',
+          src: 'https://embed.tawk.to/64b65abccc26a871b0291a35/1h5k54spo',
+          async: true,
+          defer: true,
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/64b65abccc26a871b0291a35/1h5k54spo';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+        }
       ]
     }
   },
