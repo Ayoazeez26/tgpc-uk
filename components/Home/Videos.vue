af6b75ae8bb47b0b42dff8445c6a868aca459fd9
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2023-07-28 21:25:17 +0100

    feat: added video

--- components/Home/Videos.vue
+++ components/Home/Videos.vue
@@ -4,7 +4,10 @@
   <div
     class="flex flex-col items-center mb-[100px] lg:mb-[200px] mx-auto px-4 xl:px-0 w-full max-w-[1240px]"
   >
-    <img class="w-full" src="/img/video.png" alt="video placeholder" >
+    <video ref="video" controls loop class="mt-10">
+      <source src="/assets/Crystalize.mp4" type="video/mp4">
+      <p>Your browser does not support HTML video, here's a <a href="/assets/Crystalize.mp4">link </a> to watch the video</p>
+    </video>
   </div>
 </template>
 
