3db29cd15c03337c81397d96c3d896e8ea1f0942
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-11-21 15:39:56 +0100

    Fixed active states

--- components/Dashboard/AccountSidebar.vue
+++ components/Dashboard/AccountSidebar.vue
@@ -1,39 +1,3 @@
-<!-- <template>
-  <div
-    class="flex flex-col md:space-y-5 space-y-2 md:w-[15%] md:border-b-none border-b-2 md:pb-0 pb-5 md:h-screen md:fixed"
-  >
-    <nuxt-link
-      to="/account"
-      :class="{
-        'bg-black text-white py-3 text-left px-5 rounded-md outline-none':
-          currentRoute === '/account',
-        'text-black py-3 text-left px-5 rounded-md outline-none':
-          currentRoute !== '/account',
-      }"
-    >
-      Account
-    </nuxt-link>
-
-    <nuxt-link
-      to="/account#manage-plan"
-      :class="{
-        'bg-black text-white py-3 text-left px-5 rounded-md outline-none':
-          currentRoute === '/account#manage-plan',
-        'text-black py-3 text-left px-5 rounded-md outline-none':
-          currentRoute !== '/account#manage-plan',
-      }"
-    >
-      Manage Plan
-    </nuxt-link>
-  </div>
-</template>
-
-<script setup>
-const route = useRoute();
-const currentRoute = route.href;
-console.log(route.hash);
-</script> -->
-
 <template>
   <div
     class="flex flex-col md:space-y-5 space-y-2 md:w-[15%] md:border-b-none border-b-2 md:pb-0 pb-5 md:h-screen md:fixed"
--- components/Dashboard/Header.vue
+++ components/Dashboard/Header.vue
@@ -6,8 +6,10 @@ export default {};
   <header
     class="flex items-center justify-between border-b md:px-20 px-5 py-5 md:fixed md:top-0 md:w-full bg-grey-3"
   >
-    <p class="md:text-base text-sm">Dashboard</p>
-    <img src="/svg/logo.svg" alt="" />
+    <nuxtlink to="/dashboard">
+      <p class="md:text-base text-sm">Dashboard</p></nuxtlink
+    >
+    <nuxtlink to="/dashboard"> <img src="/svg/logo.svg" alt="" /></nuxtlink>
     <div></div>
   </header>
 </template>
